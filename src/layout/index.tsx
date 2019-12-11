aece5413f80dbdb6515c102890e0683c0dff224a
MANSOUL <xiaohu12347@gmail.com>
2019-12-11 23:06:38 +0800

    feat: resize content

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,22 +2,37 @@ import React from 'react'
 import Editor from '@src/components/editor'
 import FileTree from '@src/components/fileTree'
 import './index.less'
+import VerticalResizer from './verticalResizer'
 
 export default function Layout() {
+  const refTreeElementWidth = React.useRef(0)
+  const refTreeElement = React.useRef<HTMLDivElement | null>(null)
   const refEditor = React.useRef(null)
 
   React.useEffect(() => {
     console.log(refEditor)
+    if (refTreeElement.current) {
+      refTreeElementWidth.current = refTreeElement.current.clientWidth
+    }
   }, [])
+
+  const handleResizerChange = (offset: number) => {
+    if (refTreeElement.current) {
+      const nextWidth = refTreeElementWidth.current + offset
+      refTreeElement.current.style.width = `${nextWidth}px`
+      refTreeElementWidth.current = nextWidth
+    }
+  }
 
   return (
     <div className="webcode-layout">
       <div className="webcode-layout__toolbar"></div>
       <div className="webcode-layout__container">
         <div className="webcode-layout__extensions"></div>
-        <div className="webcode-layout__filetree">
+        <div className="webcode-layout__filetree" ref={refTreeElement}>
           <FileTree />
         </div>
+        <VerticalResizer onChange={handleResizerChange} />
         <div className="webcode-layout__content">
           <Editor ref={refEditor} />
         </div>
