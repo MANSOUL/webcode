eae36379177da7e06a728332abbd065c5bf1ce1f
MANSOUL <xiaohu12347@gmail.com>
2019-12-11 22:08:13 +0800

    feat: conponent interface

---
+++ src/components/fileTree/interface.tsx
@@ -0,0 +1,21 @@
+export interface FileTreeFile {
+  id: string
+  name: string
+  children: FileTreeFile[]
+  type: 'folder' | 'file'
+}
+
+export interface FileTreeFolderProps {
+  initalOpen?: boolean
+  name: string
+  level?: number
+  files: FileTreeFile[]
+}
+
+export interface FileTreeFileProps {
+  name: string
+  type?: 'folder' | 'folderOpen' | 'file'
+  level?: number
+  active?: boolean
+  onClick?: () => void
+}
