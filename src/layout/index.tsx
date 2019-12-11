fd2bd38425147a552cbbe658a0ba6b5f399e9e8b
MANSOUL <xiaohu12347@gmail.com>
2019-12-11 23:37:51 +0800

    feat: resizer terminal height

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,16 +3,22 @@ import Editor from '@src/components/editor'
 import FileTree from '@src/components/fileTree'
 import './index.less'
 import VerticalResizer from './verticalResizer'
+import HorizontalResizer from './horizontalResizer'
 
 export default function Layout() {
   const refTreeElementWidth = React.useRef(0)
   const refTreeElement = React.useRef<HTMLDivElement | null>(null)
+  const refTerminalElementHeight = React.useRef(0)
+  const refTerminalElement = React.useRef<HTMLDivElement | null>(null)
   const refEditor = React.useRef(null)
 
   React.useEffect(() => {
     console.log(refEditor)
     if (refTreeElement.current) {
       refTreeElementWidth.current = refTreeElement.current.clientWidth
+    }
+    if (refTerminalElement.current) {
+      refTerminalElementHeight.current = refTerminalElement.current.clientHeight
     }
   }, [])
 
@@ -21,6 +27,14 @@ export default function Layout() {
       const nextWidth = refTreeElementWidth.current + offset
       refTreeElement.current.style.width = `${nextWidth}px`
       refTreeElementWidth.current = nextWidth
+    }
+  }
+
+  const handleTerminalReisizerChange = (offset: number) => {
+    if (refTerminalElement.current) {
+      const nextHeight = refTerminalElementHeight.current - offset
+      refTerminalElement.current.style.height = `${nextHeight}px`
+      refTerminalElementHeight.current = nextHeight
     }
   }
 
@@ -37,7 +51,11 @@ export default function Layout() {
           <div className="webcode-layout__code">
             <Editor ref={refEditor} />
           </div>
-          <div className="webcode-layout__terminal"></div>
+          <HorizontalResizer onChange={handleTerminalReisizerChange} />
+          <div
+            ref={refTerminalElement}
+            className="webcode-layout__terminal"
+          ></div>
         </div>
       </div>
       <div className="webcode-layout__footbar"></div>
