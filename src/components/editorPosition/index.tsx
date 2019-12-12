5774cbb0ac531004950799fb213ee921a8082b68
MANSOUL <xiaohu12347@gmail.com>
2019-12-12 15:12:48 +0800

    feat: editor position add selected col count

--- src/components/editorPosition/index.tsx
+++ src/components/editorPosition/index.tsx
@@ -4,13 +4,17 @@ import React from 'react'
 export interface Props {
   row: number
   col: number
+  selected?: number
 }
 
-export default function EditorPosition({ row, col }: Props) {
+export default function EditorPosition({ row, col, selected = 0 }: Props) {
   return (
     <div className="webcode-editor-pos">
       <span className="webcode-editor-pos__item">行 {row}</span>,&nbsp;
       <span className="webcode-editor-pos__item">列 {col}</span>
+      {selected > 0 ? (
+        <span className="webcode-editor-pos__item">(已选择 {selected})</span>
+      ) : null}
     </div>
   )
 }
