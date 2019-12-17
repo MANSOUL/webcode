d69a73b5135fbaaffb2097e3256b7549a0119381
MANSOUL <xiaohu12347@gmail.com>
2019-12-17 19:22:03 +0800

    项目状态结构

--- src/store/project/actions.ts
+++ src/store/project/actions.ts
@@ -4,10 +4,18 @@ import mFetch from '@src/utils/mFetch'
 export const FETCH_PROJECT_START = 'FETCH_PROJECT_START'
 export const FETCH_PROJECT_DONE = 'FETCH_PROJECT_DONE'
 export const FETCH_PROJECT_ERROR = 'FETCH_PROJECT_ERROR'
+export const FETCH_FILE_START = 'FETCH_FILE_START'
+export const FETCH_FILE_DONE = 'FETCH_FILE_DONE'
+export const FETCH_FILE_ERROR = 'FETCH_FILE_ERROR'
 
 export interface ProjectAction extends Action {
   type: string
-  payload?: any
+  payload?: {
+    fileStructure: any
+    fileContent: string
+    relative: string
+    errorMessage: string
+  }
 }
 
 /**
@@ -24,7 +32,7 @@ export const fetchProject = (project: string) => {
       if (res.status === 200) {
         dispatch({
           type: FETCH_PROJECT_DONE,
-          payload: { data: res.data.fileTree }
+          payload: { fileStructure: res.data.fileTree }
         })
       } else {
         dispatch({
@@ -37,6 +45,42 @@ export const fetchProject = (project: string) => {
     } catch (error) {
       dispatch({
         type: FETCH_PROJECT_ERROR,
+        payload: {
+          errorMessage: error.message
+        }
+      })
+    }
+  }
+}
+
+/**
+ * 获取文件内容
+ * @param project
+ * @param relative
+ */
+export const fetchFile = (project: string, relative: string) => {
+  return async (dispatch: Dispatch) => {
+    dispatch({
+      type: FETCH_FILE_START
+    })
+    try {
+      const res = await mFetch(`/api/file/${project}?relative=${relative}`)
+      if (res.status === 200) {
+        dispatch({
+          type: FETCH_FILE_DONE,
+          payload: { fileContent: res.data.content, relative }
+        })
+      } else {
+        dispatch({
+          type: FETCH_FILE_ERROR,
+          payload: {
+            errorMessage: res.errorMessage
+          }
+        })
+      }
+    } catch (error) {
+      dispatch({
+        type: FETCH_FILE_ERROR,
         payload: {
           errorMessage: error.message
         }
--- src/store/project/index.ts
+++ src/store/project/index.ts
@@ -3,45 +3,80 @@ import {
   FETCH_PROJECT_DONE,
   FETCH_PROJECT_ERROR,
   FETCH_PROJECT_START,
+  FETCH_FILE_DONE,
+  FETCH_FILE_ERROR,
+  FETCH_FILE_START,
   ProjectAction
 } from './actions'
 
 export interface ProjectState {
   loading: boolean
   error: boolean
-  errorMessage: string
-  data?: any
+  errorMessage: string | undefined
+  fileStructure: any
+  fileContents: Record<string, string>
+  currentFile: string
 }
 
 const initialState: ProjectState = {
   loading: false,
   error: false,
-  errorMessage: ''
+  errorMessage: '',
+  fileStructure: null,
+  fileContents: {},
+  currentFile: ''
+}
+
+const getPayload = (action: ProjectAction) => {
+  return (
+    action.payload || {
+      errorMessage: '',
+      fileStructure: {},
+      relative: '',
+      fileContent: ''
+    }
+  )
 }
 
 const reducer: Reducer<ProjectState> = (
   state: ProjectState = initialState,
   action: ProjectAction
 ) => {
+  const payload = getPayload(action)
   switch (action.type) {
     case FETCH_PROJECT_START:
       return {
+        ...state,
         loading: true,
         error: false,
         errorMessage: ''
       }
     case FETCH_PROJECT_ERROR:
       return {
+        ...state,
         loading: false,
         error: true,
-        errorMessage: action.payload.errorMessage
+        errorMessage: payload.errorMessage
       }
     case FETCH_PROJECT_DONE:
       return {
+        ...state,
         loading: false,
         error: false,
         errorMessage: '',
-        data: action.payload.data
+        fileStructure: payload.fileStructure
+      }
+    case FETCH_FILE_DONE:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        errorMessage: '',
+        fileContents: {
+          ...state.fileContents,
+          [payload.relative]: payload.fileContent
+        },
+        currentFile: payload.relative
       }
     default:
       return state
