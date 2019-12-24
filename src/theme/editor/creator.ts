a1bbc8b138deacd3cd568181a95483fed5461a65
MANSOUL <xiaohu12347@gmail.com>
2019-12-24 18:47:27 +0800

    editor 定制theme

--- src/theme/editor/creator.ts
+++ src/theme/editor/creator.ts
@@ -6,25 +6,46 @@ export const EDITOR_THEME_CSS_NAME = `.${EDITOR_THEME_NAME}`
 const isDark = true
 
 export default function themeCreator(theme: Theme) {
-  const tokenColors = theme.tokenColors
+  const { colors, tokenColors } = theme
   return `
   ${EDITOR_THEME_CSS_NAME} {
-    background-color: ${theme.colors['editor.background']};
-    color: ${theme.colors['editor.foreground']}
+    background-color: ${colors['editor.background']};
+    color: ${colors['editor.foreground']}
   }
   ${EDITOR_THEME_CSS_NAME} .ace_gutter {
-    background: ${theme.colors['editor.background']}
-    color: ${theme.colors['editorLineNumber.foreground']}
+    background: ${colors['editor.background']};
+    color: ${colors['editorLineNumber.foreground']};
   }
+
+  /** scroll bar **/
+  ::-webkit-scrollbar {
+    background-color: ${colors['editor.background']};
+  }
+  ::-webkit-scrollbar-thumb
+  {
+    background-color: rgba(255,255,255,.1);
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: rgba(255,255,255,.3);
+  }
+  /** firefox **/
+  ${EDITOR_THEME_CSS_NAME} .ace_scrollbar {
+    scrollbar-color: rgba(255,255,255,.1) ${colors['editor.background']};
+    cursor: pointer;
+  }
+  ${EDITOR_THEME_CSS_NAME} .ace_scrollbar:hover {
+    scrollbar-color: rgba(255,255,255,.3) ${colors['editor.background']};
+  }
+
   ${EDITOR_THEME_CSS_NAME} .ace_print-margin {
     width: 1px;
     background: #555651
   }
   ${EDITOR_THEME_CSS_NAME} .ace_cursor {
-    color: ${theme.colors['editorCursor.foreground']}
+    color: ${colors['editorCursor.foreground']}
   }
   ${EDITOR_THEME_CSS_NAME} .ace_marker-layer .ace_selection {
-    background: #49483E
+    background: ${colors['editor.selectionBackground']};
   }
   ${EDITOR_THEME_CSS_NAME}.ace_multiselect .ace_selection.ace_start {
     box-shadow: 0 0 3px 0px #272822;
@@ -36,28 +57,42 @@ export default function themeCreator(theme: Theme) {
     margin: -1px 0 0 -1px;
     border: 1px solid #49483E
   }
+  /** 编辑器当前行 **/
   ${EDITOR_THEME_CSS_NAME} .ace_marker-layer .ace_active-line {
-    background: #202020
+    background-color: ${colors['editor.lineHighlightBackground']};
   }
+  /** 行数当前行 **/
   ${EDITOR_THEME_CSS_NAME} .ace_gutter-active-line {
-    background-color: #272727
+    background-color: ${colors['editor.lineHighlightBackground']};
   }
+  /** 当前行选中文本的遮罩 **/
   ${EDITOR_THEME_CSS_NAME} .ace_marker-layer .ace_selected-word {
-    border: 1px solid #49483E
+    border: 1px solid #49483E;
   }
   ${EDITOR_THEME_CSS_NAME} .ace_invisible {
     color: #52524d
   }
-  ${EDITOR_THEME_CSS_NAME} .ace_entity.ace_EDITOR_THEME_CSS_NAME.ace_tag,
+  ${EDITOR_THEME_CSS_NAME} .ace_entity.ace_name.ace_tag,
   ${EDITOR_THEME_CSS_NAME} .ace_keyword,
-  ${EDITOR_THEME_CSS_NAME} .ace_meta.ace_tag,
   ${EDITOR_THEME_CSS_NAME} .ace_storage {
     ${getTokenCss(tokenColors, 'keyword')}
   }
-  ${EDITOR_THEME_CSS_NAME} .ace_punctuation,
-  ${EDITOR_THEME_CSS_NAME} .ace_punctuation.ace_tag {
-    color: #fff
+  /** 标签名 **/
+  ${EDITOR_THEME_CSS_NAME} .ace_xml-pe.ace_xml.ace_doctype,
+  ${EDITOR_THEME_CSS_NAME} .ace_meta.ace_tag {
+    ${getTokenCss(tokenColors, 'punctuation.definition.tag')}
   }
+  ${EDITOR_THEME_CSS_NAME} .ace_punctuation {
+    /** ${getTokenCss(tokenColors, 'string.template')} **/
+  }
+  ${EDITOR_THEME_CSS_NAME} .ace_operator {
+    ${getTokenCss(tokenColors, 'punctuation.separator')}
+  }
+  /** <> **/
+  ${EDITOR_THEME_CSS_NAME} .ace_punctuation.ace_tag {
+    ${getTokenCss(tokenColors, 'punctuation.definition.tag')}
+  }
+
   ${EDITOR_THEME_CSS_NAME} .ace_constant.ace_character,
   ${EDITOR_THEME_CSS_NAME} .ace_constant.ace_language,
   ${EDITOR_THEME_CSS_NAME} .ace_constant.ace_numeric,
@@ -88,21 +123,30 @@ export default function themeCreator(theme: Theme) {
     font-style: italic;
     color: #66D9EF
   }
-  ${EDITOR_THEME_CSS_NAME} .ace_entity.ace_EDITOR_THEME_CSS_NAME.ace_function,
+
+  ${EDITOR_THEME_CSS_NAME} .ace_xml-pe.ace_xml,
+  ${EDITOR_THEME_CSS_NAME} .ace_entity.ace_name.ace_function,
   ${EDITOR_THEME_CSS_NAME} .ace_entity.ace_other,
-  ${EDITOR_THEME_CSS_NAME} .ace_entity.ace_other.ace_attribute-EDITOR_THEME_CSS_NAME,
+  ${EDITOR_THEME_CSS_NAME} .ace_entity.ace_other.ace_attribute-name{
+    ${getTokenCss(tokenColors, 'punctuation.definition.list')}
+  }
   ${EDITOR_THEME_CSS_NAME} .ace_variable {
-    color: #A6E22E
+    ${getTokenCss(tokenColors, 'variable')}
   }
   ${EDITOR_THEME_CSS_NAME} .ace_variable.ace_parameter {
-    font-style: italic;
-    color: #FD971F
+    ${getTokenCss(tokenColors, 'variable.parameter')}
   }
   ${EDITOR_THEME_CSS_NAME} .ace_string {
-    color: #E6DB74
+    ${getTokenCss(tokenColors, 'string.quoted')}
   }
   ${EDITOR_THEME_CSS_NAME} .ace_comment {
     ${getTokenCss(tokenColors, 'comment')}
+  }
+  ${EDITOR_THEME_CSS_NAME} .ace_name.ace_function {
+    ${getTokenCss(tokenColors, 'entity.name.function')}
+  }
+  ${EDITOR_THEME_CSS_NAME} .ace_identifier {
+    ${getTokenCss(tokenColors, 'entity.name.tag')}
   }
   ${EDITOR_THEME_CSS_NAME} .ace_indent-guide {
     background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAEklEQVQImWPQ0FD0ZXBzd/wPAAjVAoxeSgNeAAAAAElFTkSuQmCC) right repeat-y
