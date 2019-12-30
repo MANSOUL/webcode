47f395ae482856a684f572cab6fc2b524e758e34
MANSOUL <xiaohu12347@gmail.com>
2019-12-30 16:52:58 +0800

    删除不必要的文件

--- src/theme/editor/index.ts
+++ src/theme/editor/index.ts
@@ -1,17 +1,8 @@
 import useTheme from '../useTheme'
-import themeCreator, { EDITOR_THEME_NAME } from './creator'
 import insert from '../insertCss'
 
 const insertCss = insert()
 
-export const themeConfig = {
-  isDark: true,
-  cssClass: EDITOR_THEME_NAME,
-  cssText: ''
-}
-
 export default function useEditorTheme() {
   const theme = useTheme()
-  const cssText = themeCreator(theme)
-  insertCss(cssText)
 }
