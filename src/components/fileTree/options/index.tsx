f00eaf0b1f05b6eb4f3fdc6a858b27548efa77c3
MANSOUL <xiaohu12347@gmail.com>
2019-12-31 15:26:16 +0800

    feat: 文件树操作项组件

--- src/components/fileTree/options/index.tsx
+++ src/components/fileTree/options/index.tsx
@@ -5,12 +5,14 @@ import svgAddFile from '@src/aseets/svg/add-file-1.svg'
 import svgAddFolder from '@src/aseets/svg/add-folder.svg'
 import svgRename from '@src/aseets/svg/edit-file-v2.svg'
 import svgUpdate from '@src/aseets/svg/available-updates.svg'
+import svgDownload from '@src/aseets/svg/downloads-folder.svg'
 
 interface SvgProps {
   src: string
+  onClick?: () => void
 }
 
-function Svg({ src }: SvgProps) {
+function Svg({ src, onClick }: SvgProps) {
   return (
     <ReactSVG
       src={src}
@@ -19,7 +21,6 @@ function Svg({ src }: SvgProps) {
           console.error(error)
           return
         }
-        console.log(svg)
       }}
       beforeInjection={svg => {
         svg.classList.add('webcode-filetree-options__svg')
@@ -30,20 +31,43 @@ function Svg({ src }: SvgProps) {
       renumerateIRIElements={false}
       wrapper="div"
       className="webcode-filetree-options__icon"
-      onClick={() => {
-        console.log('wrapper onClick')
-      }}
+      onClick={onClick}
     />
   )
 }
 
-export default function Options() {
+export interface Props {
+  download?: boolean
+  rename?: boolean
+  addFile?: boolean
+  addFolder?: boolean
+  update?: boolean
+  onDownload?: () => void
+  onRename?: () => void
+  onAddFile?: () => void
+  onAddFolder?: () => void
+  onUpdate?: () => void
+}
+
+export default function Options({
+  download = false,
+  rename = false,
+  addFile = true,
+  addFolder = true,
+  update = false,
+  onDownload,
+  onRename,
+  onAddFile,
+  onAddFolder,
+  onUpdate
+}: Props) {
   return (
     <div className="webcode-filetree-options">
-      <Svg src={svgRename} />
-      <Svg src={svgAddFile} />
-      <Svg src={svgAddFolder} />
-      <Svg src={svgUpdate} />
+      {download ? <Svg src={svgDownload} onClick={onDownload} /> : null}
+      {rename ? <Svg src={svgRename} onClick={onRename} /> : null}
+      {addFile ? <Svg src={svgAddFile} onClick={onAddFile} /> : null}
+      {addFolder ? <Svg src={svgAddFolder} onClick={onAddFolder} /> : null}
+      {update ? <Svg src={svgUpdate} onClick={onUpdate} /> : null}
     </div>
   )
 }
