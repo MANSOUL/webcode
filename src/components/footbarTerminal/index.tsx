52eac8005fb9541df4ba7224604df4641162e528
MANSOUL <xiaohu12347@gmail.com>
2019-12-31 17:23:53 +0800

    fix: footbar terminal icon switch to svg

--- src/components/footbarTerminal/index.tsx
+++ src/components/footbarTerminal/index.tsx
@@ -1,15 +1,29 @@
 import './index.less'
 import React from 'react'
 import FootbarItem from '../footbarItem'
-
+import { ReactSVG } from 'react-svg'
+import svgTerminal from '@src/aseets/svg/terminal.svg'
+import clsx from 'clsx'
 export interface Props {
   onClick?: () => void
+  className?: string
 }
 
-export default function FootbarTerminal({ onClick }: Props) {
+export default function FootbarTerminal({ onClick, className }: Props) {
   return (
-    <FootbarItem className="webcode-fb-terminal" title="终端" onClick={onClick}>
-      <i className="iconfont icon-terminal1 webcode-fb-terminal__icon" />
+    <FootbarItem
+      className={clsx('webcode-fb-terminal', className)}
+      title="终端"
+      onClick={onClick}
+    >
+      <ReactSVG
+        src={svgTerminal}
+        beforeInjection={svg => {
+          svg.classList.add('webcode-fb-terminal__svg')
+        }}
+        wrapper="div"
+        className="webcode-fb-terminal__icon"
+      />
     </FootbarItem>
   )
 }
