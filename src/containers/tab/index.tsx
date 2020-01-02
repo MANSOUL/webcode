00dc2f8512b576d458d8b4a1ca66f687df5fd418
MANSOUL <xiaohu12347@gmail.com>
2020-01-02 17:49:18 +0800

    feat: 文件更改后关闭文件前对话框询问是否保存文件

--- src/containers/tab/index.tsx
+++ src/containers/tab/index.tsx
@@ -8,11 +8,21 @@ import Tab, {
 import { useSelector, useDispatch } from 'react-redux'
 import { AppStore } from '@src/store'
 import MyEditor from '../editor'
-import { getFileIndex } from '@src/store/files/util'
-import { changeCurrentFile, fileCloseFile } from '@src/store/files/actions'
+import { getFileIndex, getFileById } from '@src/store/files/util'
+import {
+  changeCurrentFile,
+  fileCloseFile,
+  fileSaveFile
+} from '@src/store/files/actions'
 import { FileContent } from '@src/store/files'
 import Scroller from '@src/components/ui/scroller'
 import { createStyles } from '@src/theme'
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+  DialogActions
+} from '@src/components/ui/dialog'
+import Button from '@src/components/ui/button'
 
 const useStyles = createStyles(theme => ({
   tabSwitcher: {
@@ -35,6 +45,12 @@ export default function MyTab() {
   const editor = useSelector((store: AppStore) => store.editor)
   const dispatch = useDispatch()
   const classes = useStyles()
+  const [dialog, setDialog] = React.useState({
+    open: false,
+    title: '',
+    content: ''
+  })
+  const refCurrentFile = React.useRef<FileContent | null>(null)
 
   React.useEffect(() => {
     refScroller.current && refScroller.current.resize()
@@ -50,8 +66,45 @@ export default function MyTab() {
     dispatch(changeCurrentFile(files.fileContents[index].id))
   }
 
-  const handleTabClose = (item: FileContent) => () =>
-    dispatch(fileCloseFile(item.id))
+  const handleTabClose = (item: FileContent) => () => {
+    const file = getFileById(files.fileContents, item.id)
+    if (!file) return
+    refCurrentFile.current = file
+    if (file.modified) {
+      setDialog({
+        title: `是否要保存对 ${'index.ts'} 的更改?`,
+        content: '如果不保存，更改将丢失。',
+        open: true
+      })
+    } else {
+      dispatch(fileCloseFile(item.id))
+    }
+  }
+
+  const handleUnsaveFile = () => {
+    refCurrentFile.current && dispatch(fileCloseFile(refCurrentFile.current.id))
+    setDialog({
+      ...dialog,
+      open: false
+    })
+  }
+
+  const handleCloseDialog = () => {
+    setDialog({
+      ...dialog,
+      open: false
+    })
+  }
+
+  const handleSaveFile = () => {
+    if (!refCurrentFile.current) return
+    dispatch(fileSaveFile(refCurrentFile.current.id))
+    dispatch(fileCloseFile(refCurrentFile.current.id))
+    setDialog({
+      ...dialog,
+      open: false
+    })
+  }
 
   return (
     <Tab onTabChange={handleTabChange}>
@@ -77,6 +130,21 @@ export default function MyTab() {
           </TabItem>
         ))}
       </TabContainer>
+      <Dialog open={dialog.open} transparent>
+        <DialogTitle>{dialog.title}</DialogTitle>
+        <DialogContent>{dialog.content}</DialogContent>
+        <DialogActions>
+          <Button type="danger" size="small" onClick={handleUnsaveFile}>
+            不保存
+          </Button>
+          <Button size="small" onClick={handleCloseDialog}>
+            取消
+          </Button>
+          <Button type="primary" size="small" onClick={handleSaveFile}>
+            保存
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Tab>
   )
 }
