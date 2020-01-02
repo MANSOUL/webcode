4249f4e772dfa6f35676f8d6728cc385aecdc2b4
MANSOUL <xiaohu12347@gmail.com>
2020-01-03 00:53:49 +0800

    fix: terminal scrollbar theme color

--- src/theme/terminal/creator.ts
+++ src/theme/terminal/creator.ts
@@ -4,9 +4,11 @@ const TERMINAL_CLASS_NAME = '.webcode-terminal__xterm'
 
 export default function themeCreator(theme: Theme) {
   const { colors } = theme
+  const backgroundColor =
+    colors['terminal.background'] || colors['editor.background']
   return `
     ::-webkit-scrollbar {
-      background-color: ${colors['terminal.background']};
+      background-color: ${backgroundColor};
     }
     ::-webkit-scrollbar-thumb
     {
@@ -16,10 +18,10 @@ export default function themeCreator(theme: Theme) {
       background-color: rgba(255,255,255,.3);
     }
     ${TERMINAL_CLASS_NAME} .xterm-viewport {
-      scrollbar-color: rgba(255,255,255,.1) ${colors['editor.background']};
+      scrollbar-color: rgba(255,255,255,.1) ${backgroundColor};
     }
     ${TERMINAL_CLASS_NAME} .xterm-viewport:hover {
-      scrollbar-color: rgba(255,255,255,.3) ${colors['editor.background']};
+      scrollbar-color: rgba(255,255,255,.3) ${backgroundColor};
     }
   `
 }
