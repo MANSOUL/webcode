8df710d67cdf657ad118090e760c1c0182b65a25
MANSOUL <xiaohu12347@gmail.com>
2020-01-08 14:18:21 +0800

    feat: 从query上获取当前项目

--- src/config/project.ts
+++ src/config/project.ts
@@ -1,2 +1,5 @@
-export const getProject = () => 'demo'
+import queryString from 'query-string'
+
+const qs = queryString.parse(location.search)
+export const getProject = () => qs.project
 export const TERMINAL_SOCKET_URL = 'ws://localhost:9999'
